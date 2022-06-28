22449ce55fcdb9f253f1cc9fef593577379b1118
Nadezhda Fedina <96238966+nadia3373@users.noreply.github.com>
2022-06-28 20:34:50 +0700

    Update Task1.py

--- Week1/Task1.py
+++ Week1/Task1.py
@@ -15,7 +15,7 @@ def main():
 
     domain = domain_name(url)
     if not domain == 1:
-        print("domain name = " + domain)
+        print("domain name = " + str(domain))
     else:
         print("Not a valid URL")
 
@@ -24,14 +24,40 @@ def domain_name(url):
     # Проверка, начинается ли строка с http(s)://, http(s)://www.
     url_start = "^\"?(https?:\/\/www\.|https?:\/\/|www\.)"
     match = re.search(url_start, url)
-    # Если проверка пройдена, удалить проверяемую часть и получить доменное имя
+    # Если проверка пройдена, удалить проверяемую часть
     if match is not None:
         domain = re.sub(match.group(), '', url)
-        domain = domain.split(".")[0]
-        return domain
-    # Если проверка не пройдена, сообщить, что ссылка в некорректном формате
     else:
-        return 1
+        domain = url
+    # Проверить, есть ли в ссылке символы /
+    if "/" in domain:
+        domain = domain.split("/")
+        domain = domain[0]
+    domain = domain.split(".")
+    temp = []
+    # Для простых доменов
+    if len(domain) == 2:
+        return domain[0]
+    else:
+        temp = []
+        # Для составных доменов удалить короткие элементы и перепроверить длину
+        for i in range(len(domain)):
+            if len(domain[i]) < 3:
+                temp.append(domain[i])
+        if temp:
+            for i in range(len(temp)):
+                domain.remove(temp[i])
+        if len(domain) == 1:
+            return domain[0]
+        else:
+            temp = []
+            for i in range(len(domain)):
+                if len(domain[i]) == 3:
+                    temp.append(domain[i])
+            if len(temp) > 1:
+                return domain[0]
+            else:
+                return domain[len(domain) - 1]
 
 if __name__ == "__main__":
     main()

413fa4461c8dac74691d1938dc6f14880b26a5a1
Nadezhda Fedina <3773373@mail.ru>
2022-07-13 22:29:45 +0700

    Update Task2.py

--- Week3/Task2.py
+++ Week3/Task2.py
@@ -11,17 +11,15 @@ import time
 def decorator(call_count, start_sleep_time, factor, border_sleep_time):
     def delay_function(func):
         def wrapper(number):
-            print(call_count)
-            count = 1
+            count = 0
             sleep_time = start_sleep_time
             while count <= call_count:
                 # Расчёт времени ожидания для текущей итерации.
-                if count > 1:
-                    if sleep_time < border_sleep_time:
-                        sleep_time = sleep_time * 2 ** factor
-                    if sleep_time >= border_sleep_time:
-                        sleep_time = border_sleep_time
-                else:
+                if sleep_time < border_sleep_time:
+                    sleep_time = start_sleep_time * factor ** count
+                if sleep_time >= border_sleep_time:
+                    sleep_time = border_sleep_time
+                if count == 0:
                     print("Кол-во запусков = " + str(call_count))
                     print("Начало работы")
                 # Ожидание.
@@ -29,7 +27,7 @@ def decorator(call_count, start_sleep_time, factor, border_sleep_time):
                 # Вызов декорируемой функции.
                 result = func(number)
                 # Печать результата.
-                print("Запуск номер " + str(count) + ". Ожидание: " +
+                print("Запуск номер " + str(count + 1) + ". Ожидание: " +
                       str(sleep_time) + " секунд. Результат декорируемой "
                       "функции = " + str(result))
                 count += 1
@@ -38,7 +36,7 @@ def decorator(call_count, start_sleep_time, factor, border_sleep_time):
     return delay_function
 
 
-@decorator(call_count=10, start_sleep_time=0.01, factor=1, border_sleep_time=2)
+@decorator(call_count=5, start_sleep_time=1, factor=2, border_sleep_time=7)
 def multiplier(number: int):
     return number * 2
 

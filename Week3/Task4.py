586401b0cec3911de51fe2bfe38c8a7abbe6f530
Nadezhda Fedina <3773373@mail.ru>
2022-07-13 16:27:27 +0700

    Update Task4.py

--- Week3/Task4.py
+++ Week3/Task4.py
@@ -1,24 +1,28 @@
-"""
-Надо написать класс CyclicIterator.
-Итератор должен итерироваться по итерируемому объекту
-(list, tuple, set, range, Range2, и т. д.), и когда достигнет последнего
-элемента, начинать сначала.
-"""
-
 from itertools import cycle
 
 
 class CyclicIterator:
     def __init__(self, container):
-        self.container = cycle(container)
+        # Убедиться, что объект является итерируемым.
+        if hasattr(container, '__iter__'):
+            self.container = cycle(container)
+        else:
+            print("Объект не является итерируемым.")
+            exit()
 
     def __iter__(self):
         return self
 
     def __next__(self):
+        # Вернуть следующий элемент.
         for item in self.container:
             return item
 
 cyclic_iterator = CyclicIterator(range(3))
 for i in cyclic_iterator:
-    print(i)
+    # Убедиться, что объект не пуст.
+    if i is None:
+        print("Итерируемый объект пуст.")
+        break
+    else:
+        print(i)
